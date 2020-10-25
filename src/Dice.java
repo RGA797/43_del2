3bc10eb9f2616115b2bad36d925e59801d5b0b6f
Muaz <muaz@outlook.dk>
2020-10-25 19:32:30 +0100

    Started work on diceTotal and rollDice

--- src/Dice.java
+++ src/Dice.java
@@ -1,42 +1,34 @@
 
 public class Dice {
 
-    public static int[] RollDicePlayer1() {
-        //Method that returns an array with 2 random numbers between 1 and 6. (Player 1)
-        int[] Player1Dice = new int[2];
+//    public static int[] RollDicePlayer1() {
+//        //Method that returns an array with 2 random numbers between 1 and 6. (Player 1)
+//        int[] Player1Dice = new int[2];
+//
+//        int diceCount = 2;
+//        int maxEye = 6;
+//        int minEye = 1;
+//        int range = maxEye - minEye + 1;
+//
+//        Player1Dice[0] = (int)(Math.random()*range) + minEye;
+//        Player1Dice[1] = (int)(Math.random()*range) + minEye;
+//
+//        //System.out.println(Player1Dice[0]);
+//        //System.out.println(Player1Dice[1]);
+//        return Player1Dice;
+//    }
 
-        int diceCount = 2;
+    public int rollDice() {
         int maxEye = 6;
         int minEye = 1;
-        int range = maxEye - minEye + 1;
 
-        Player1Dice[0] = (int)(Math.random()*range) + minEye;
-        Player1Dice[1] = (int)(Math.random()*range) + minEye;
-
-        //System.out.println(Player1Dice[0]);
-        //System.out.println(Player1Dice[1]);
-        return Player1Dice;
-
-    }
-    public static int[] RollDicePlayer2() {
-        // Method that returns an array with 2 random numbers between 1 and 6. (Player 2)
-        int[] Player2Dice = new int[2];
-
-        int diceCount = 2;
-        int maxEye = 6;
-        int minEye = 1;
-        int range = maxEye - minEye + 1;
-
-        Player2Dice[0] = (int) (Math.random() * range) + minEye;
-        Player2Dice[1] = (int) (Math.random() * range) + minEye;
-        return Player2Dice;
+        return (int) ((Math.random() * maxEye) + minEye);
     }
 
-    /*public static void main(String[] args) {
+    public int diceTotal() {
+        int roll1 = rollDice();
+        int roll2 = rollDice();
 
-        System.out.println(RollDicePlayer1()[0]);
-        System.out.println(RollDicePlayer1()[0]);
-        System.out.println(RollDicePlayer1()[0]);
-        System.out.println(RollDicePlayer1()[0]);
-    } */
+        return roll1 + roll2;
+    }
 }
