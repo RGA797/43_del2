df611f52a085b6bde85d65cb97597bca0dbe940c
Muaz <muaz@outlook.dk>
2020-10-25 19:31:14 +0100

    Added playercount, get & set player position methods

--- src/Game.java
+++ src/Game.java
@@ -5,5 +5,10 @@ import java.util.Scanner;
             Player Player_1 = new Player(1, "bob");
             System.out.println(Player_1.getPlayerName());
             System.out.println(Player_1.NewPlayerName("joe"));
+            System.out.println(Player_1.getPlayerPosition());
+            // Set a players position to 4, default is 2 when the game starts.
+            Player_1.setPlayerPosition(4);
+            // Get player position (help function for board)
+            System.out.println(Player_1.getPlayerPosition());
         }
     }
--- src/Player.java
+++ src/Player.java
@@ -1,19 +1,39 @@
 public class Player {
-    int playerCount = 2;
+    // User choses player type (player 1/player 2)
     int playerType;
     String playerName;
     String n_name; // new name
     int playerPosition;
 
+    // The total count of players.
+    public int playerCount() {
+        return 2;
+    }
+
+    // Player object, this is set when the game starts.
     public Player(int playerType, String playerName) {
         this.playerType = playerType;
         this.playerName = playerName;
+        this.playerPosition = 2;
     }
+
     public String getPlayerName() {
         return playerName;
     }
+
+    // Set a players name to something the user specified.
     public String NewPlayerName(String n_name) {
         playerName = n_name;
         return playerName;
+    }
+
+    // Set a players position, default is 2 when the game starts.
+    public void setPlayerPosition(int pos) {
+        playerPosition = pos;
+    }
+
+    // Get player position (help function for board)
+    public int getPlayerPosition() {
+        return playerPosition;
     }
 }
