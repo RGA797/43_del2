764889dae1dda8e45d65dc803b2328fee3b9fed9
Rachid131 <55383115+Rachid131@users.noreply.github.com>
2020-10-25 19:43:00 +0100

    added functionality to Account class

--- src/Account.java
+++ src/Account.java
@@ -1,5 +1,5 @@
 public class Account {
-    int totalBalance;
+    int totalBalance = 0;
     int balanceToWin = 3000;
     int change;
 
@@ -9,5 +9,16 @@ public class Account {
     }
     public void changeBalance(int change){
         totalBalance = totalBalance + change;
+    }
+    public int getTotalBalance(){
+        return totalBalance;
+    }
+    public boolean winCondition(){
+        if(totalBalance >= balanceToWin){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
--- src/Spil.java
+++ src/Spil.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
     public class Spil {
         public static void main(String[] args) {
             Scanner in = new Scanner(System.in);
-            Player Player_1 = new Player(1, "bob");
-            System.out.println(Player_1.getPlayerName());
-            System.out.println(Player_1.NewPlayerName("joe"));
+            Account Account_1 = new Account(0,3000);
+            System.out.println(Account_1.getTotalBalance());
+            Account_1.changeBalance(300);
+            System.out.println(Account_1.getTotalBalance());
         }
     }
