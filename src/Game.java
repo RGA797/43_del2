4487c7c8cdbf00f1f4f8b3cee0e003c4e6bb7972
Rachid131 <55383115+Rachid131@users.noreply.github.com>
2020-10-26 20:08:20 +0100

    Added a switch

--- src/Game.java
+++ src/Game.java
@@ -9,9 +9,33 @@ public class Game {
             Account account2 = new Account();
             Board piece1 = new Board();
             Board piece2 = new Board();
+            System.out.println("Are you player 1 or player 2?");
+            int playerType = in.nextInt();
+            String playername1;
+            String playername2;
+            switch (playerType){
+                case playerType = 1:
+                    System.out.println("skriv dit navn");
+                    playername1 = in.nextLine();
+                    System.out.println("Write player2's name");
+                    playername2 = in.nextLine();
+                    break;
+                case playerType = 2:
+                    System.out.println("Write player 2's name");
+                    playername1 = in.nextLine();
+                    System.out.println("Write player 1 name");
+                    playername2 = in.nextLine();
+                    break;
+            }
 
             int playerType = in.nextInt();
+            System.out.println("What is" +  " "+playerType+'s' + " name?" );
+            if (playerType = 1){
+                String playername1 = in.nextLine()
+            }
+            if (playerType = 2){
 
+            }
             String playerName = in.nextLine();
 
             Player player1 = new Player(playerType,playerName);
